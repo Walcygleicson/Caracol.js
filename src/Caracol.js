a0e9ec4ea3671982e9dc5eac2996cf3ffba60cc6
Walcygleicson <walcygleicsonoliveira@gmail.com>
2025-03-23 18:07:41 -0300

    Documentação inicial do método Caracol.newLog Método retorna uma nova função personalisada que pode ser usada como console.log()

---
+++ src/Caracol.js
@@ -0,0 +1,27 @@
+
+const Caracol = {}
+
+/**
+ * @typedef {object} LineLogOptions Objeto define configurações sobre linha de registro.
+ * @prop {boolean} [getLineNumber] Se *`true`* obtém o número da linha onde a função foi invocada. O padrão é *`true`*.
+ * @prop {string | false} [sep] Define uma *string* que separa o número da linha do restante do texto. O padrão é *`" >> "`*. Se definido como *`false`* apenas um espaço será usado como separador.
+ * 
+ * 
+ * @typedef {object} StyleOptions
+ * @prop {} [color] O nome da cor de texto.
+ * @prop {} [background] O nome da cor de fundo.
+ * @prop {} [font] O nome do estilo da fonte de texto.
+ * @prop {} [reset] Reseta a formatação para o padrão do terminal ao final da *`string`*
+ * 
+ * @typedef {StyleOptions | LineLogOptions} LogInitOptions
+ */
+
+/**
+ * * Retorna uma nova função personalisada que imprime um dado no terminal com as configurações previamente fornecidas no objeto *`logInit`*
+ * ---
+ * @param {LogInitOptions} logInit Um objeto de inicialização da função personalisada.
+ */
+Caracol.newLog = function(logInit){}
+
+
+export default Caracol
